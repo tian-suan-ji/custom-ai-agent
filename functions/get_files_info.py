b5d5d23fdd3ab961266573ec90788fb84dbc7c81
Tian suan Ji <royxalvarez@gmail.com>
2026-06-03 12:33:13 -0500

    debug commonpath and syntax
    Fix incorrect syntax and spelling
    
    change common validation condition to point
    at absolute path
    
    debug commonpath argument from incorrect
    syntax to putting str into iter
    cause ValueError to be thrown

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,21 +2,24 @@
 import os
 
 def get_files_info(working_directory: str, directory: str = ".") -> str:
+    
     try:
         if not os.path.isdir(directory):
             return f"\"{directory}\" is not a directory"
 
         working_directory_absolute = os.path.abspath(working_directory)
+        
         full_directory = os.path.join(working_directory_absolute, directory)
+        
         target_directory = os.path.normpath(full_directory)
         
-        valid_common_directory = os.paath.commonpath(working_directory, target_directory)
-        if valid_common_directory != working_directory:
+        valid_common_directory = os.path.commonpath([working_directory_absolute, target_directory])
+        
+        if valid_common_directory != working_directory_absolute:
             return f"Error: Cannot list \"{directory}\" as it is outside the permitted working directory"
         
         if os.path.exists(target_directory):
-            return f"\"{target_directory}\" is within the working directory"
+            return f"Success: \"{directory}\" is within the working directory"
     
     except Exception as e:
         return f"Error: {e}"
-
