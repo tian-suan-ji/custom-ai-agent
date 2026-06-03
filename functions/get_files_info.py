e0df8251159caf459b84815c7c81662a7ae399e2
Tian suan Ji <royxalvarez@gmail.com>
2026-06-03 10:46:08 -0500

    validate directory access

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,6 +2,10 @@
 import os
 
 def get_files_info(working_directory: str, directory: str = ".") -> str:
-    working_directory = os.path.abspath(working_directory)
-    directory = os.path.join(working_directory, directory)
-    target_path = os.path.normpath(directory)
+    working_directory_absolute = os.path.abspath(working_directory)
+    full_directory = os.path.join(working_directory_absolute, directory)
+    target_directory = os.path.normpath(full_directory)
+    
+    valid_common_directory = os.paath.commonpath(working_directory, target_directory)
+    if valid_common_directory != working_directory:
+        return f"Error: Cannot list \"{directory}\" as it is outside the permitted working directory"
