9b1e7602684420287082c4ba8757d4262fe6c7e6
Tian suan Ji <royxalvarez@gmail.com>
2026-06-04 08:21:39 -0500

    expand tests

--- test_get_files_info.py
+++ test_get_files_info.py
@@ -3,9 +3,9 @@ from functions.get_files_info import get_files_info
 
 print(get_files_info("calculator", "."))
 
-print(get_files_info("calculator", "/bin"))
+print(get_files_info("calculator", "pkg"))
+
+print(get_files_info("calculator", "/bin    "))
 
 print(get_files_info("calculator", "../"))
-
-print(get_files_info("calculator", "main.py"))
 
