8cfa83dda545ddec44f14a4712418a3278d0116b
Tian suan Ji <royxalvarez@gmail.com>
2026-06-04 08:00:38 -0500

    access target directory items and metadata

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -17,7 +17,17 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         
         if valid_common_directory != working_directory_absolute:
             return f"Error: Cannot list \"{directory}\" as it is outside the permitted working directory"
-        
+        # get files in target path
+        directory_contents = ""
+        for item in os.listdir(target_directory):
+            #item = os.path.abspath(item)
+            item_abs = os.path.join(full_directory, item)
+            
+            directory_contents += f"{item}: file_size={os.path.getsize(item_abs)} bytes, is_dir={os.path.isdir(item_abs)}"
+            if item != os.listdir(target_directory)[-1]:
+                directory_contents += "\n"
+            
+        print(directory_contents)
         if os.path.exists(target_directory):
             return f"Success: \"{directory}\" is within the working directory"
     
