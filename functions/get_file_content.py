aa987a26c97bad4bbb65dc5f52f4aa20f78805bb
Tian suan Ji <royxalvarez@gmail.com>
2026-06-04 10:36:23 -0500

    validate common paths and rename parameter

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -2,10 +2,16 @@
 import os
 
 
-def get_file_content(working_directory: str, directory: str) -> str:
+def get_file_content(working_directory: str, file_path: str) -> str:
     working_directory_absolute = os.path.abspath(working_directory)
-    file_path = os.path.join(working_directory_absolute, directory)
+    full_file_path = os.path.join(working_directory_absolute, file_path)
     
-    if not os.path.isfile(file_path):
-        return f"Errorr: Cannot read \"{directory}\" as it is outside the permitted directory"
+    if not os.path.isfile(full_file_path):
+        return f"Errorr: Cannot read \"{file_path}\" as it is outside the permitted directory"
+    
+    target_file = os.path.normpath(full_file_path)
+    
+    valid_common_directory = os.path.commonpath([working_directory_absolute, target_file])
+    if valid_common_directory != working_directory_absolute:
+        return f"Error: File not found or is not a regular file {file_path}"
     
