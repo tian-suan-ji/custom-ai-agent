cc363f5e5d03d6dec8bfd81f7e8e6c5f58186141
Tian suan Ji <royxalvarez@gmail.com>
2026-06-05 12:34:19 -0500

    limit access to file content and catch errors
    temporary hardcoded maximum content
    for which the agent can access
    
    catch any unexpected errors
    now returns a string gracefully

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,26 +1,31 @@
 
-
 import os
-import sys
-sys.path.append('..')
-from config import MAX_CHARACTERS
+
 
 def get_file_content(working_directory: str, file_path: str) -> str:
-    working_directory_absolute = os.path.abspath(working_directory)
-    full_file_path = os.path.join(working_directory_absolute, file_path)
-    
-    if not os.path.isfile(full_file_path):
-        return f"Errorr: Cannot read \"{file_path}\" as it is outside the permitted directory"
-    
-    target_file = os.path.normpath(full_file_path)
-    
-    valid_common_directory = os.path.commonpath([working_directory_absolute, target_file])
-    if valid_common_directory != working_directory_absolute:
-        return f"Error: File not found or is not a regular file {file_path}"
-    
-    # read file
-    with open(target_file, 'r', encoding="UTF-8") as f:
-        content = f.read(MAX_CHARACTERS)
-        print(content)
+    try:
+        working_directory_absolute = os.path.abspath(working_directory)
+        full_file_path = os.path.join(working_directory_absolute, file_path)
+        
+        if not os.path.isfile(full_file_path):
+            return f"Errorr: Cannot read \"{file_path}\" as it is outside the permitted directory"
+        
+        target_file = os.path.normpath(full_file_path)
+        
+        valid_common_directory = os.path.commonpath([working_directory_absolute, target_file])
+        if valid_common_directory != working_directory_absolute:
+            return f"Error: File not found or is not a regular file {file_path}"
+        
+        # read file
+        with open(target_file, 'r', encoding="UTF-8") as f:
+            content = f.read(10_000)
+            extra_content = f.read(10_001)
+            if extra_content[-1] != '':
+                content += f"[...File \"{file_path}\" truncated at 10_000 characters]"
+            print(content)
+    except Exception as e:
+        return f"Error: {e}"
+
 
 get_file_content("../calculator", "main.py")
+
