e54d25db6ce6f3709cfa45789dc09529591fbe7a
Tian suan Ji <royxalvarez@gmail.com>
2026-06-05 18:01:50 -0500

    debug directory checks and expand tests
    fix write file function
    cause child directories to fail
    
    expand test cases

--- functions/write_file.py
+++ functions/write_file.py
@@ -5,30 +5,26 @@ import os
 def write_file(working_directory: str, file_path: str, content: str) -> str:
     try:
         working_directory_absolute = os.path.abspath(working_directory)
-        absolute_file_path = os.path.join(working_directory_absolute, file_path)
-
-        if not os.path.isfile(absolute_file_path):
-            return f"Errorr: Cannot read \"{file_path}\" as it is outside the permitted directory"
+        full_file_path = os.path.join(working_directory_absolute, file_path)
         
-        # normalize path
-        target_file_path = os.path.normpath(absolute_file_path)
+        if os.path.isdir(full_file_path):
+            return f"Errorr: Cannot write to \"{file_path}\" as it is outside the permitted directory"
         
-        if os.path.isdir(target_file_path):
-            return f"Error: Cannot write to \"{file_path}\" as as it is a directory"
+        target_file = os.path.normpath(full_file_path)
         
-        # create any missing parent directories
-        os.makedirs(target_file_path, exist_ok=True)
-        
-        # common absolute path
-        common_path = os.path.commonpath([working_directory_absolute, target_file_path])
-
-        if common_path != working_directory_absolute:
+        valid_common_directory = os.path.commonpath([working_directory_absolute, target_file])
+        if valid_common_directory != working_directory_absolute:
             return f"Error: File not found or is not a regular file {file_path}"
+        # second second validation
+# create any missing parent directories
+        if not os.path.exists(os.path.dirname(target_file)):
+            os.makedirs(target_file, exist_ok=True)
         
         # write to file
-        with open(target_file_path, 'w', encoding="UTF-8") as f:
-            new_content = f.write(content)
+        with open(target_file, 'w', encoding="UTF-8") as f:
+            f.write(content)
             
-        return f"Successfully wrote to \"{file_path}\" {len(new_content)} characters written"
+            
+            return f"Successfully wrote to \"{file_path}\" ({len(content)} characters written)"
     except Exception as e:
         return f"Error: {e}"
