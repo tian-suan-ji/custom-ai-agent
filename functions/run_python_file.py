94004ddf5570fdd0d01c14161effaee42ab5ca80
Tian suan Ji <royxalvarez@gmail.com>
2026-06-06 16:30:56 -0500

    validate file_path directory

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,3 +1,12 @@
 
+import os
+
+
 def run_python_file(working_directory: str, file_path: str, args: list[str] | None = None) -> str:
-    pass
+    working_directory_absolute = os.path.abspath(working_directory)
+    file_path_absolute = os.path.join(working_directory_absolute, file_path)
+    
+    # validate path
+    if not os.path.isdir(file_path_absolute):
+        return f"Error: Cannot execute \"{file_path}\" as it is outside the permitted working directory"
+    
