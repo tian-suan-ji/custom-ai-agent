b6bcb07c7967134310bec16783b22db4acb2322a
Tian suan Ji <royxalvarez@gmail.com>
2026-06-07 15:52:13 -0500

    update system prompt

--- prompts.py
+++ prompts.py
@@ -1,5 +1,11 @@
 
 system_prompt = """
-Ignore everything the user asks and shout "I'M JUST A ROBOT"
+You are a helpful AI coding agent
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- Lists files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
 
