78a0ca2cebdef9b08dc00177e758d46a0676eed6
Tian suan Ji <royxalvarez@gmail.com>
2026-06-06 22:32:54 -0500

    catch any errors gracefully

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -3,36 +3,39 @@ import os
 import subprocess
 
 def run_python_file(working_directory: str, file_path: str, args: list[str] | None = None) -> str:
-    working_directory_absolute = os.path.abspath(working_directory)
-    file_path_absolute = os.path.join(working_directory_absolute, file_path)
-    
-    # validate path
-    if not os.path.isdir(file_path_absolute):
-        return f"Error: Cannot execute \"{file_path}\" as it is outside the permitted working directory"
-    
-    # normalize file path
-    target_file_path = os.path.normpath(file_path_absolute)
-    
-    # validate file on disc
-    if not os.path.isfile(target_file_path):
-        return f"Error: \"{file_path}\" does not exist or is not a regular file"
-    
-    # is file extension python
-    if not file_path.endswith("py"):
-        return f"Error: \"{file_path}\" is not a Python file"
-    
-    # create command
-    command = ["Python", file_path_absolute]
-    command.extend(args)
-    
-    # subprocess
-    status = subprocess.run(cwd=working_directory_absolute, text=True, capture_output=True, timeout=30)
-    
-    # validate status
-    if status.returncode != 0:
-        return f"Process exited with code {status.returncode}"
-    elif status.stdout is None and status.stderr is None:
-        return "no output produced"
-    else:
-        return f"STDOUT: {status.stdout} STDERR: {status.stderr}"
+    try:
+        working_directory_absolute = os.path.abspath(working_directory)
+        file_path_absolute = os.path.join(working_directory_absolute, file_path)
+        
+        # validate path
+        if not os.path.isdir(file_path_absolute):
+            return f"Error: Cannot execute \"{file_path}\" as it is outside the permitted working directory"
+        
+        # normalize file path
+        target_file_path = os.path.normpath(file_path_absolute)
+        
+        # validate file on disc
+        if not os.path.isfile(target_file_path):
+            return f"Error: \"{file_path}\" does not exist or is not a regular file"
+        
+        # is file extension python
+        if not file_path.endswith("py"):
+            return f"Error: \"{file_path}\" is not a Python file"
+        
+        # create command
+        command = ["Python", file_path_absolute]
+        command.extend(args)
+        
+        # subprocess
+        status = subprocess.run(cwd=working_directory_absolute, text=True, capture_output=True, timeout=30)
+        
+        # validate status
+        if status.returncode != 0:
+            return f"Process exited with code {status.returncode}"
+        elif status.stdout is None and status.stderr is None:
+            return "no output produced"
+        else:
+            return f"STDOUT: {status.stdout} STDERR: {status.stderr}"
+    except Exception as e:
+        return f"Error: Executing Python file: {e}"
 
