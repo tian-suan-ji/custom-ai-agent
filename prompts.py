dd0835d859499f307649f7524f21453560e201a9
Tian suan Ji <royxalvarez@gmail.com>
2026-06-08 10:03:06 -0500

    implement more allowable operations to agent

--- prompts.py
+++ prompts.py
@@ -6,6 +6,11 @@ When a user asks a question or makes a request, make a function call plan. You c
 
 - Lists files and directories
 
+- Read file contents
+
+- Execute Python files with optional arguments
+- Write or overwrite files
+
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
 
