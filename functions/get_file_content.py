233d1f9135aa35cd9526b433cb1314f4c2117364
Tian suan Ji <royxalvarez@gmail.com>
2026-06-09 04:29:38 -0500

    rearrange validation
    fix error spelling containing double r
    
    fix validation arrangement
    cause error when agent caalls the function

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -22,15 +22,14 @@ def get_file_content(working_directory: str, file_path: str) -> str:
         working_directory_absolute = os.path.abspath(working_directory)
         full_file_path = os.path.join(working_directory_absolute, file_path)
         
-        if not os.path.isdir(full_file_path):
-            return f"Errorr: Cannot read \"{file_path}\" as it is outside the permitted directory"
-        
         target_file = os.path.normpath(full_file_path)
         
         valid_common_directory = os.path.commonpath([working_directory_absolute, target_file])
         if valid_common_directory != working_directory_absolute:
-            return f"Error: File not found or is not a regular file {file_path}"
+            return f"Error: Cannot read \"{file_path}\" as it is outside the permitted directory"
         
+        if not os.path.isfile(target_file):
+            return f"Error: File not found or is not a regular file {file_path}"
         # read file
         with open(target_file, 'r', encoding="UTF-8") as f:
             content = f.read(10_000)
