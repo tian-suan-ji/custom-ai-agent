7ac1b7a8fac234fc364e1c200885781d26fa383c
Tian suan Ji <royxalvarez@gmail.com>
2026-06-12 04:15:07 -0500

    refactor and fix code
    update schema description
    
    fix spelling
    
    arrange validations
    and added parent directory validator
    
    fix error messages
    
    change return statement position

--- functions/write_file.py
+++ functions/write_file.py
@@ -5,7 +5,7 @@ from google.genai import types
 
 schema_write_file = types.FunctionDeclaration(
     name="write_file",
-    description="write to a specified file, providing the revisions",
+    description="write to a specified file",
     parameters=types.Schema(
         type=types.Type.OBJECT,
         required=["file_path", "content"],
@@ -27,24 +27,25 @@ def write_file(working_directory: str, file_path: str, content: str) -> str:
         working_directory_absolute = os.path.abspath(working_directory)
         full_file_path = os.path.join(working_directory_absolute, file_path)
         
-        if os.path.isdir(full_file_path):
-            return f"Errorr: Cannot write to \"{file_path}\" as it is outside the permitted directory"
-        
         target_file = os.path.normpath(full_file_path)
         
         valid_common_directory = os.path.commonpath([working_directory_absolute, target_file])
         if valid_common_directory != working_directory_absolute:
-            return f"Error: File not found or is not a regular file {file_path}"
-        # second second validation
-# create any missing parent directories
-        if not os.path.exists(os.path.dirname(target_file)):
-            os.makedirs(target_file, exist_ok=True)
+            return f"Error: Cannot write to \"{file_path}\" as it is outside the permitted working directory"
+        
+        # check if file is a directory
+        if os.path.isdir(target_file):
+            return f"Error: Cannot write to \"{file_path}\" as it is a directory"
+        
+        # create any missing parent directories
+        parent_directory = os.path.dirname(target_file)
+        os.makedirs(parent_directory, exist_ok=True)
         
         # write to file
         with open(target_file, 'w', encoding="UTF-8") as f:
             f.write(content)
             
             
-            return f"Successfully wrote to \"{file_path}\" ({len(content)} characters written)"
+        return f"Successfully wrote to \"{file_path}\" ({len(content)} characters written)"
     except Exception as e:
         return f"Error: {e}"
