7c596a98e2c1c678750f4b8cd58af16b4ab48f8f
Tian suan Ji <royxalvarez@gmail.com>
2026-06-12 03:57:53 -0500

    refactor and fix code
    remove needless validation check
    
    fix extension validation by adding dot
    
    rrearrange common path validator
    
    fix command not pointing to target file path
    
    fix validation for expected output
    and output now a list of strings
    
    return output instead of None
    
    Fix case issues

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -32,26 +32,24 @@ def run_python_file(working_directory: str, file_path: str, args: list[str] | No
         working_directory_absolute = os.path.abspath(working_directory)
         file_path_absolute = os.path.join(working_directory_absolute, file_path)
         
-        # validate path
-        if os.path.isdir(file_path_absolute):
-            return f"Error: Cannot execute \"{file_path}\" as it is outside the permitted working directory"
         
         # normalize file path
         target_file_path = os.path.normpath(file_path_absolute)
         
-        # validate file on disc
-        if not os.path.isfile(target_file_path):
-            return f"Error: \"{file_path}\" does not exist or is not a regular file"
         valid_common_directory = os.path.commonpath([working_directory_absolute, target_file_path])
         if valid_common_directory != working_directory_absolute:
             return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
         
+        # validate file on disc
+        if not os.path.isfile(target_file_path):
+            return f"Error: \"{file_path}\" does not exist or is not a regular file"
+        
         # is file extension python
-        if not file_path.endswith("py"):
+        if not file_path.endswith(".py"):
             return f"Error: \"{file_path}\" is not a Python file"
         
         # create command
-        command = ["python", file_path_absolute]
+        command = ["python", target_file_path]
         if args is not None:
             command.extend(args)
         
@@ -59,12 +57,17 @@ def run_python_file(working_directory: str, file_path: str, args: list[str] | No
         status = subprocess.run(command, cwd=working_directory_absolute, text=True, capture_output=True, timeout=30)
         
         # validate status
+        output = []
         if status.returncode != 0:
-            return f"Process exited with code {status.returncode}"
-        elif status.stdout is None and status.stderr is None:
-            return "no output produced"
-        else:
-            return f"STDOUT: {status.stdout} STDERR: {status.stderr}"
+            output.append(f"Process exited with code {status.returncode}")
+        if not status.stdout and not status.stderr:
+            output.append("No output produced")
+        
+        if status.stdout:
+            output.append(f"STDOUT:\n{status.stdout}")
+        if status.stderr:
+            output.append(f"STDERR:\n{status.stderr}")
+        return '\n'.join(output)
     except Exception as e:
-        return f"Error: Executing Python file: {e}"
+        return f"Error: executing Python file: {e}"
 
